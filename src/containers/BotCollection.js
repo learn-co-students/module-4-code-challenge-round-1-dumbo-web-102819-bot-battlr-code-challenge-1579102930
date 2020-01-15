905d88bf99a63d59b75db2d125ca8b512c18ed5e
Carlie Anglemire <carlieanglemire@Carlies-MacBook-Air.local>
2020-01-15 11:52:02 -0500

    first commit

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -2,14 +2,18 @@ import React from "react";
 import BotCard from "../components/BotCard";
 
 class BotCollection extends React.Component {
-  //your code here
+  
+	renderingBots= () => {
+		return this.props.robots.map(robot => {
+			return <BotCard bot={robot} handleClick={this.props.handleClick}/>
+		})
+	}
 
   render(){
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
+			  {this.renderingBots()}
     		</div>
   	  </div>
   	);
