736a3b513c5fdbba832d39b2b9ef31b1c474033a
GAierken <g.aierken@gmail.com>
2020-01-15 11:04:24 -0500

    render all bots and your army area

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,14 +1,20 @@
 import React from "react";
 import BotCard from "../components/BotCard";
+import uuid from 'uuid'
 
 class BotCollection extends React.Component {
-  //your code here
-
+  
+renderBot = () => {
+	return this.props.bots.map((bot, index) => {
+		return <BotCard key={uuid()} bot={bot}/>
+	})
+}
   render(){
+	  
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
+    		  {this.renderBot()}
     		  Collection of all bots
     		</div>
   	  </div>
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,23 @@
 import React from "react";
+import BotCollection from './BotCollection'
+import YourBotArmy from "./YourBotArmy";
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+  state = {
+    bots: []
+  }
+
+  componentDidMount(){
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+    .then(r => r.json())
+    .then(botsArr => this.setState({ bots: botsArr}))
+  }
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy/>
+        <BotCollection bots={this.state.bots}/>
       </div>
     );
   }
