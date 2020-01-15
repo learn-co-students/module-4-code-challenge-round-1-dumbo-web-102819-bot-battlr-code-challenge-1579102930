81c324bb0be3e8dc854159c20352cbe9d344095e
Griffin Poole <griffinpoole5@gmail.com>
2020-01-15 10:53:18 -0500

    bots fetch working and setting state

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -3,6 +3,20 @@ import React from "react";
 class BotsPage extends React.Component {
   //start here with your code for step one
 
+  state = {
+    bots: []
+  }
+
+  componentDidMount(){
+    fetch("https://bot-battler-api.herokuapp.com/api/v1/bots")
+    .then(r => r.json())
+    .then((bots) => {
+      this.setState({
+        bots: bots
+      }, ()=>{console.log(this.state)})
+    })
+  }
+
   render() {
     return (
       <div>
